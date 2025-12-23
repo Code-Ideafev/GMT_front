8f305245231c20ed37d21e343bc70e727709b36f
doyeon36 <s25003@gsm.hs.kr>
2025-12-23 17:23:44 +0900

    feat : "타이머 및 마이페이지 UI 수정"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Timer from "./timer/timer"; 
+import Timer from "./Timer/Timer"; 
 import Login from './pages/Login';
 import MyPage from "./MyPage/my-page";
 import EditProfilePage from "./MyPage/EditProfilePage"; 
