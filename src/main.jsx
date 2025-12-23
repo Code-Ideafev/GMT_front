d853eeb4fa240f19935acceb149f0ae572ebdf87
Ye-1ulx <s25014@gsm.hs.kr>
2025-12-23 10:44:11 +0900

    feat: 모든 충돌 해결 및 폴더 구조 통합 완료

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom"; // 라우터 기능 유지
-import App from "./App";
+import App from "../App";
 import "./index.css";
 
 // 두 코드의 장점만 합친 최종 형태입니다.
