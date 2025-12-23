9d94bd6f2c80ee6293ac7f9cabfbfce5ad418f4f
Ye-1ulx <s25014@gsm.hs.kr>
2025-12-23 11:26:11 +0900

    라우터 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom"; // 라우터 기능 유지
-import App from "../App";
-import "./index.css";
+import App from "./App";
+// import "./index.css";
 
 // 두 코드의 장점만 합친 최종 형태입니다.
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
       <App />
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
