72579e3ad869484d3db49a613adb6d43e870204b
Ye-1ulx <s25014@gsm.hs.kr>
2025-12-29 08:15:05 +0900

    임시커밋

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://172.16.1.34:8080', 
+  baseURL: 'http://192.168.1.23:8080', 
   timeout: 5000,
   headers: { 'Content-Type': 'application/json' },
 });
